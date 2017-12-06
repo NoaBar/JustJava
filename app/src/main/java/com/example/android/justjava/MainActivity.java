fc96d46ce2871bf9060c3e0834563d91bd5e86b3
Noah Bar <noahbar@gmail.com>
2017-12-06 11:05:49 +0100

    "display" is now "displayQuantity". string "thank you" is not appearing after rotation.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,12 +1,4 @@
 
-/**
- * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
- * This is the package name our example uses:
- *
- * package com.example.android.justjava;
- *
- */
-
 package com.example.android.justjava;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -34,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         quantity = quantity + 1 ;
-        display (quantity);
+        displayQuantity (quantity);
     }
 
     /**
@@ -44,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
        if (quantity != 0) {
            quantity = quantity - 1;
        }
-        display (quantity);
+        displayQuantity (quantity);
     }
 
 
@@ -61,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
@@ -97,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         quantity=savedInstanceState.getInt("quantity",0);
-        display(quantity);
+        displayQuantity(quantity);
         displayPrice(quantity*5);
     }
 }
